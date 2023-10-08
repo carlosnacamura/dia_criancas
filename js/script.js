c90ed40b0e186d39737abd281d2d15fab7cf5b3a
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2023-10-08 18:02:21 +0000

    Cursor diferenciado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-addEventListener("keydown",(evt)=>{
-    if(evt.key === "Enter"){
-        
-    }
+const position = {
+    x: 0,
+    y: 0
+}
+const cursor = document.querySelector(".circulo")
+addEventListener("mousemove",(evt)=>{
+    position.x = evt.clientX
+    position.y = evt.clientY
+    cursor.style.left = position.x + "px"
+    cursor.style.top = position.y + "px"
+})
+addEventListener("mousedown",()=>{
+    cursor.style.background = "lightblue"
+})
+addEventListener("mouseup",()=>{
+    cursor.style.background = ""
 })
