d9d4486cc2af91568c05b298464ab95e0265b0b0
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2023-10-09 15:50:10 +0000

    Transição de scroll

--- js/script.js
+++ js/script.js
@@ -16,10 +16,12 @@ addEventListener("mousedown",()=>{
 addEventListener("mouseup",()=>{
     cursor.style.background = ""
 })
+
 /* Jogo de adivinhar número */
 const numero_sort = Math.floor(Math.random() * 101)
 const numero_tent = document.querySelector("input[type=number]")
 const titulo_numeroSort = document.querySelector(".titulo_numeroSort")
+const div_jogos = document.querySelector(".jogos")
 function tentarSorte(){
     if(numero_tent.value == numero_sort){
         titulo_numeroSort.textContent = "PARABÉNS VOCÊ ACERTOU!!!!"
@@ -29,6 +31,7 @@ function tentarSorte(){
         titulo_numeroSort.textContent = "Seu número é maior que o sorteado"
     }
 }
+
 addEventListener("keydown",(evt)=>{
     if(evt.key === "Enter"){
         tentarSorte()
