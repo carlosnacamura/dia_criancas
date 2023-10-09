f043c19168f496e5621481d3deeb10e6f8a22afc
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2023-10-09 13:51:05 +0000

    Jogo feito

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Cursor diferenciado */
 const position = {
     x: 0,
     y: 0
@@ -14,4 +15,22 @@ addEventListener("mousedown",()=>{
 })
 addEventListener("mouseup",()=>{
     cursor.style.background = ""
+})
+/* Jogo de adivinhar número */
+const numero_sort = Math.floor(Math.random() * 101)
+const numero_tent = document.querySelector("input[type=number]")
+const titulo_numeroSort = document.querySelector(".titulo_numeroSort")
+function tentarSorte(){
+    if(numero_tent.value == numero_sort){
+        titulo_numeroSort.textContent = "PARABÉNS VOCÊ ACERTOU!!!!"
+    } else if (numero_tent.value < numero_sort){
+        titulo_numeroSort.textContent = "Seu número é menor que o sorteado"
+    } else if (numero_tent.value > numero_sort){
+        titulo_numeroSort.textContent = "Seu número é maior que o sorteado"
+    }
+}
+addEventListener("keydown",(evt)=>{
+    if(evt.key === "Enter"){
+        tentarSorte()
+    }
 })
